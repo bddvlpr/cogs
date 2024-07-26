520633d1f29620c0a4f76f41372719f75c549eb1
Luna Simons <luna@bddvlpr.com>
2024-07-26 13:26:26 +0200

    chore: create flake and schemat check

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    schemat.url = "github:bddvlpr/schemat";
+    schemat.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        pkgs,
+        lib,
+        inputs',
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+
+        checks = {
+          format-schemat = let
+            inherit (inputs'.schemat.packages) schemat;
+          in
+            pkgs.stdenvNoCC.mkDerivation {
+              name = "format-schemat-check";
+              src = ./.;
+
+              buildInputs = [schemat];
+
+              buildPhase = ''
+                mkdir $out
+                schemat -c "**/*.scm"
+              '';
+            };
+        };
+      };
+    };
+}
